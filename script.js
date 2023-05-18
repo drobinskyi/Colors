abf5728bc1c7e73194158e613453901724418adf
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-05-18 21:54:39 +0300

    Connected chroma.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', event => {
+    if (event.code.toLowerCase() === 'space') {
+        setRandomColors();
+    };
+})
+
+// Функція генерації випадкового кольору
 function generateRandomColor() {
     const hexCodes = '0123456789ABCDEF';
     let color = '';
@@ -10,10 +17,23 @@ function generateRandomColor() {
     return '#' + color;
 }
 
-function setrandomColors() {
+function setRandomColors() {
     cols.forEach((col) => {
-        col.style.background = generateRandomColor();
+        const text = col.querySelector('h2');
+        const button = col.querySelector('button');
+        const color = chroma.random();
+
+        text.textContent = color;
+        col.style.background = color;
+
+        setTextColor(text, color);
+        setTextColor(button, color);
     });
 };
 
-setrandomColors();
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+setRandomColors();
